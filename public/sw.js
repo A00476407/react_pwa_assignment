e4d9fb592c12b5735f4fc4ee1d65b5e90140b341
Carmen K.M. Leung <carmenleung@swirecocacola.com>
2024-02-07 08:02:03 +0800

    Update service worker

--- public/sw.js
+++ public/sw.js
@@ -2,10 +2,14 @@ let cacheData='app';
 this.addEventListener("install", (event)=> {
     event.waitUntil(
         caches.open(cacheData).then((cache)=>{
-                cache.addAll([
-                'static/js/bundle.js',
+            cache.addAll([
                 '/',
-                'index.html'
+                'index.html',
+                'manifest.json',
+                'favicon.ico',
+                'logo192.png',
+                'logo512.png',
+                '/static/js/bundle.js'
             ])
         })
     )
@@ -13,11 +17,26 @@ this.addEventListener("install", (event)=> {
 
 this.addEventListener("fetch",(event)=>{
     event.respondWith(
-        caches.match(event.request).then((resp)=>{
-            if (resp)
+        caches.match(event.request).then((response)=>{
+            if (response)
             {
-                return resp
+                return response;
             }
-        })
+            
+            return fetch(event.request).then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(cacheData)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+      })
     )
 })
